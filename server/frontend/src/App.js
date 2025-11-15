3b1c4c66e941eef3c014fea24d6af8d9e8516569
seang673 <seangriffiths673@gmail.com>
2025-11-15 01:33:59 -0500

    User managment with React FrontEnd

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,17 @@
 import LoginPanel from "./components/Login/Login"
+import RegisterPanel from "./components/Register/Register"
+import HeaderPanel from "./components/Header/Header"
+import DealersPanel from "./components/Dealers/Dealers"
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel/>} />
+      <Route path="/header" element={<HeaderPanel />} />
+      <Route path="/dealers" element={<DealersPanel/>} />
     </Routes>
   );
 }
