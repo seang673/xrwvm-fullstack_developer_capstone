a313b7bd1add831600216a8001ed29f6f6fdf7ff
seang673 <seangriffiths673@gmail.com>
2025-11-16 22:12:23 -0500

    Dynamic Pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,16 +3,19 @@ import RegisterPanel from "./components/Register/Register"
 import HeaderPanel from "./components/Header/Header"
 import DealersPanel from "./components/Dealers/Dealers"
 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<RegisterPanel/>} />
-      <Route path="/header" element={<HeaderPanel />} />
-      <Route path="/dealers" element={<DealersPanel/>} />
-    </Routes>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<RegisterPanel/>} />
+            <Route path="/header" element={<HeaderPanel />} />
+            <Route path="/dealers" element={<DealersPanel/>} />
+        </Routes>
+    </BrowserRouter>
+    
   );
 }
 export default App;
