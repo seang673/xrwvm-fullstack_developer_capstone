81b9d70aeae84193f66bfdc7fed2f41b6b101a93
seang673 <seangriffiths673@gmail.com>
2025-12-02 23:48:18 -0500

    Second Module

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,18 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register";
+import DealersPanel from "./components/Dealers/Dealers";
+import HeaderPanel from "./components/Header/Header";
+
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<DealersPanel />} />
+      <Route path="/header" element={<HeaderPanel />} />
     </Routes>
   );
 }
