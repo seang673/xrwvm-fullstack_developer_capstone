7a56480a547ed5b40f961a8bdaaaff07ecaad999
seang673 <seangriffiths673@gmail.com>
2025-12-15 22:25:20 -0500

    backend fixes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import RegisterPanel from "./components/Register/Register";
 import DealersPanel from "./components/Dealers/Dealers";
 import HeaderPanel from "./components/Header/Header";
 import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 import { Routes, Route } from "react-router-dom";
 
@@ -14,6 +15,7 @@ function App() {
       <Route path="/dealers" element={<DealersPanel />} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/header" element={<HeaderPanel />} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
